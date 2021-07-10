f45c61e768b11c6a1c3924c908eab121cbc6a363
unknown <rnrudxo2872@naver.com>
2021-07-10 12:44:34 +0900

    api server에서 막혀 server 만들기로 결정

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,22 @@
 import React from "react";
+import { gql } from 'apollo-boost'
+import { useQuery } from "@apollo/client";
 
-export default () => <h1>Home</h1>;
+const  GET_MOVIE = gql(`
+    {
+        movies {
+            id
+            medium_cover_image
+        }
+    }
+`)
+
+export default function Home() {
+    const {loading, error, data,} = useQuery(GET_MOVIE)
+    if(loading){
+        return "loading..."
+    }
+    if(data && data.movie){
+        return data.movies.map(m => <h1>{m.id}</h1>);
+    }
+}
